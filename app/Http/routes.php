4c5c95ba9292fb8ac1d60bdc6691c3db16403517
dilbadil <abdillah308@gmail.com>
2015-08-26 23:32:08 +0700

    setting template & twitter api

---
+++ app/Http/routes.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+Route::get('/admin', function(){
+    return view('layouts.adminlte');
+});
+Route::get('twitter-login-callback', function(Illuminate\Http\Request $request) {
+    
+    if (! $request->has('oauth_verifier')) return "Access denied !";
+
+    $oauthVerifier = $request->input('oauth_verifier');
+    $connection = new Abraham\TwitterOAuth\TwitterOAuth(env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET'), session('oauth_token'), session('oauth_token_secret'));
+    $accessToken = $connection->oauth("oauth/access_token", array("oauth_verifier" => $oauthVerifier));
+
+    session(['access_token' => $accessToken]);
+
+    return redirect('twitter-profile');
+});
+
+Route::get('/twitter-login', function(Illuminate\Http\Request $request) {
+
+    $connection = new Abraham\TwitterOAuth\TwitterOAuth( env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET') );
+    $requestToken = $connection->oauth('oauth/request_token', array('oauth_callback' => "http://localhost:8000/twitter-login-callback"));
+
+    session([
+        'oauth_token' => $requestToken['oauth_token'],
+        'oauth_token_secret' => $requestToken['oauth_token_secret']
+    ]);
+
+    $authorizeUrl = $connection->url('oauth/authorize', ['oauth_token' => session('oauth_token')]);
+
+    return "<a href='$authorizeUrl'>Authorize on twitter</a>";
+});
+
+Route::get('/twitter-profile', function() {
+    $accessToken = session('access_token');
+
+    if (! session()->has('twitter_account')) {
+        $connection = new Abraham\TwitterOAuth\TwitterOAuth(env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET'), $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+        $twitterAccount = $connection->get("account/verify_credentials");
+        session([
+            'twitter_account' => $twitterAccount
+        ]);
+    }
+
+    $twitterAccount = session('twitter_account');
+
+    return view('twitter.index', compact('twitterAccount'));
+});
+
+Route::post('/twitter-status', ['as' => 'twitter_status_update', function(Illuminate\Http\Request $request) {
+    $connection = new Abraham\TwitterOAuth\TwitterOAuth(env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET'), session('access_token.oauth_token'), session('access_token.oauth_token_secret'));
+    $statues = $connection->post("statuses/update", ["status" => $request->input('status')]);
+
+    if ($connection->getLastHttpCode() == 200) {
+        return redirect('twitter-profile')
+            ->with("message", "Update status success!")
+            ->with("success", true);
+    }
+
+    return redirect('twitter-profile')
+        ->with('message', 'Error: can"t update status')
+        ->with('success', false);
+}]);
+
+Route::post('/twitter-send-message', ['as' => 'twitter_send_message', function(Illuminate\Http\Request $request){
+    $connection = new Abraham\TwitterOAuth\TwitterOAuth(env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET'), session('access_token.oauth_token'), session('access_token.oauth_token_secret'));
+    $fields =  [
+        'message' => $request->input('message'),
+        'to' => $request->input('to'),
+    ];
+    $request = $connection->post("direct_messages/new", $fields);
+
+    if ($connection->getLastHttpCode() == 200) {
+        return redirect('twitter-profile')
+            ->with('message', "Message sent to @" . $request->input('to'))
+            ->with('success', true);
+    }
+
+    return redirect('twitter-profile')
+        ->with('message', "Send message failed!")
+        ->with('success', false);
+}]);
---
+++ resources/views/twitter/index.blade.php
@@ -0,0 +1,246 @@
+@extends('layouts.adminlte')
+
+@section('title', 'Dashboard')
+
+@section('user_image', $twitterAccount->profile_image_url)
+
+@section('content')
+    @if (session('message'))
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                {{ session('message') }}
+            </div>
+        @else
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                {{ session('message') }}
+            </div>
+        @endif
+    @endif
+
+    <div class="row">
+        <div class="col-md-3">
+
+          <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="{{ $twitterAccount->profile_image_url }}" alt="User profile picture">
+              <h3 class="profile-username text-center">{{ $twitterAccount->name }}</h3>
+              <p class="text-muted text-center">{{ "@" . $twitterAccount->screen_name }}</p>
+
+              <ul class="list-group list-group-unbordered">
+                <li class="list-group-item">
+                  <b>Followers</b> <a class="pull-right">1,322</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Following</b> <a class="pull-right">543</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Friends</b> <a class="pull-right">13,287</a>
+                </li>
+              </ul>
+
+              <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+
+          <!-- About Me Box -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">About Me</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+              <strong><i class="fa fa-book margin-r-5"></i>  Education</strong>
+              <p class="text-muted">
+                B.S. in Computer Science from the University of Tennessee at Knoxville
+              </p>
+
+              <hr>
+
+              <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
+              <p class="text-muted">Malibu, California</p>
+
+              <hr>
+
+              <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
+              <p>
+                <span class="label label-danger">UI Design</span>
+                <span class="label label-success">Coding</span>
+                <span class="label label-info">Javascript</span>
+                <span class="label label-warning">PHP</span>
+                <span class="label label-primary">Node.js</span>
+              </p>
+
+              <hr>
+
+              <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+        </div><!-- /.col -->
+        <div class="col-md-9">
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+              <li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
+              <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
+              <li><a href="#settings" data-toggle="tab">Settings</a></li>
+            </ul>
+            <div class="tab-content">
+              <div class="active tab-pane" id="activity">
+                <!-- Post -->
+                <div class="post">
+
+                    <form class="form-horizontal" method="POST" action="{{ route('twitter_status_update') }}">
+                        {!! csrf_field() !!}
+                        <div class="form-group margin-bottom-none">
+                          <div class="col-sm-9">
+                            <input name="status" class="form-control input-sm" placeholder="Status">
+                          </div>                          
+                          <div class="col-sm-3">
+                            <button type="submit" class="btn btn-danger pull-right btn-block btn-sm">Update</button>
+                          </div>                          
+                        </div>                        
+                    </form>
+
+                </div><!-- /.post -->
+              </div><!-- /.tab-pane -->
+              <div class="tab-pane" id="timeline">
+                <!-- The timeline -->
+                <ul class="timeline timeline-inverse">
+                  <!-- timeline time label -->
+                  <li class="time-label">
+                    <span class="bg-red">
+                      10 Feb. 2014
+                    </span>
+                  </li>
+                  <!-- /.timeline-label -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-envelope bg-blue"></i>
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+                      <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+                      <div class="timeline-body">
+                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                        weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                        jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                        quora plaxo ideeli hulu weebly balihoo...
+                      </div>
+                      <div class="timeline-footer">
+                        <a class="btn btn-primary btn-xs">Read more</a>
+                        <a class="btn btn-danger btn-xs">Delete</a>
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-user bg-aqua"></i>
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+                      <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request</h3>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-comments bg-yellow"></i>
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
+                      <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+                      <div class="timeline-body">
+                        Take me to your leader!
+                        Switzerland is small and neutral!
+                        We are more like Germany, ambitious and misunderstood!
+                      </div>
+                      <div class="timeline-footer">
+                        <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline time label -->
+                  <li class="time-label">
+                    <span class="bg-green">
+                      3 Jan. 2014
+                    </span>
+                  </li>
+                  <!-- /.timeline-label -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-camera bg-purple"></i>
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+                      <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+                      <div class="timeline-body">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <li>
+                    <i class="fa fa-clock-o bg-gray"></i>
+                  </li>
+                </ul>
+              </div><!-- /.tab-pane -->
+
+              <div class="tab-pane" id="settings">
+                <form class="form-horizontal">
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+                    <div class="col-sm-10">
+                      <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-danger">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div><!-- /.tab-pane -->
+            </div><!-- /.tab-content -->
+          </div><!-- /.nav-tabs-custom -->
+        </div><!-- /.col -->
+    </div>
+
+@endsection
