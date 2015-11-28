be03b49b38ea002733a7d278bff9a47f1e0f0f41
widastarry <wida.skydev@gmail.com>
2015-11-29 06:02:15 +0700

    box

--- public/javascript/show_chart.js
+++ public/javascript/show_chart.js
@@ -13,9 +13,7 @@ $(function () {
   // This will get the first returned node in the jQuery collection.
   // var areaChart = new Chart(areaChartCanvas);
 
-  var areaChartData = {
-    labels: ["Mei","Juni","Juli","Agustus","September", "Oktober", "November"],
-    datasets: [
+  var data1 = [
       {
         label: "Penebangan Liar",
         fillColor: "rgba(210, 214, 222, 1)",
@@ -66,136 +64,19 @@ $(function () {
         pointHighlightStroke: "rgba(60,141,188,1)",
         data: [58, 58, 50, 59, 66, 87, 90]
       }
-    ]
+    ];
+
+  var areaChartData = {
+    labels: ["Mei","Juni","Juli","Agustus","September", "Oktober", "November"],
+    datasets: data1
   };
-
-  // var areaChartOptions = {
-  //   //Boolean - If we should show the scale at all
-  //   showScale: true,
-  //   //Boolean - Whether grid lines are shown across the chart
-  //   scaleShowGridLines: false,
-  //   //String - Colour of the grid lines
-  //   scaleGridLineColor: "rgba(0,0,0,.05)",
-  //   //Number - Width of the grid lines
-  //   scaleGridLineWidth: 1,
-  //   //Boolean - Whether to show horizontal lines (except X axis)
-  //   scaleShowHorizontalLines: true,
-  //   //Boolean - Whether to show vertical lines (except Y axis)
-  //   scaleShowVerticalLines: true,
-  //   //Boolean - Whether the line is curved between points
-  //   bezierCurve: true,
-  //   //Number - Tension of the bezier curve between points
-  //   bezierCurveTension: 0.3,
-  //   //Boolean - Whether to show a dot for each point
-  //   pointDot: false,
-  //   //Number - Radius of each point dot in pixels
-  //   pointDotRadius: 4,
-  //   //Number - Pixel width of point dot stroke
-  //   pointDotStrokeWidth: 1,
-  //   //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-  //   pointHitDetectionRadius: 20,
-  //   //Boolean - Whether to show a stroke for datasets
-  //   datasetStroke: true,
-  //   //Number - Pixel width of dataset stroke
-  //   datasetStrokeWidth: 2,
-  //   //Boolean - Whether to fill the dataset with a color
-  //   datasetFill: true,
-  //   //String - A legend template
-  //   legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-  //   //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-  //   maintainAspectRatio: true,
-  //   //Boolean - whether to make the chart responsive to window resizing
-  //   responsive: true
-  // };
-
-  // //Create the line chart
-  // areaChart.Line(areaChartData, areaChartOptions);
-
-  //-------------
-  //- LINE CHART -
-  //--------------
-  // var lineChartCanvas = $("#lineChart").get(0).getContext("2d");
-  // var lineChart = new Chart(lineChartCanvas);
-  // var lineChartOptions = areaChartOptions;
-  // lineChartOptions.datasetFill = false;
-  // lineChart.Line(areaChartData, lineChartOptions);
-
-  // //-------------
-  // //- PIE CHART -
-  // //-------------
-  // // Get context with jQuery - using jQuery's .get() method.
-  // var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-  // var pieChart = new Chart(pieChartCanvas);
-  // var PieData = [
-  //   {
-  //     value: 700,
-  //     color: "#f56954",
-  //     highlight: "#f56954",
-  //     label: "Chrome"
-  //   },
-  //   {
-  //     value: 500,
-  //     color: "#00a65a",
-  //     highlight: "#00a65a",
-  //     label: "IE"
-  //   },
-  //   {
-  //     value: 400,
-  //     color: "#f39c12",
-  //     highlight: "#f39c12",
-  //     label: "FireFox"
-  //   },
-  //   {
-  //     value: 600,
-  //     color: "#00c0ef",
-  //     highlight: "#00c0ef",
-  //     label: "Safari"
-  //   },
-  //   {
-  //     value: 300,
-  //     color: "#3c8dbc",
-  //     highlight: "#3c8dbc",
-  //     label: "Opera"
-  //   },
-  //   {
-  //     value: 100,
-  //     color: "#d2d6de",
-  //     highlight: "#d2d6de",
-  //     label: "Navigator"
-  //   }
-  // ];
-  // var pieOptions = {
-  //   //Boolean - Whether we should show a stroke on each segment
-  //   segmentShowStroke: true,
-  //   //String - The colour of each segment stroke
-  //   segmentStrokeColor: "#fff",
-  //   //Number - The width of each segment stroke
-  //   segmentStrokeWidth: 2,
-  //   //Number - The percentage of the chart that we cut out of the middle
-  //   percentageInnerCutout: 50, // This is 0 for Pie charts
-  //   //Number - Amount of animation steps
-  //   animationSteps: 100,
-  //   //String - Animation easing effect
-  //   animationEasing: "easeOutBounce",
-  //   //Boolean - Whether we animate the rotation of the Doughnut
-  //   animateRotate: true,
-  //   //Boolean - Whether we animate scaling the Doughnut from the centre
-  //   animateScale: false,
-  //   //Boolean - whether to make the chart responsive to window resizing
-  //   responsive: true,
-  //   // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-  //   maintainAspectRatio: true,
-  //   //String - A legend template
-  //   legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-  // };
-  // //Create pie or douhnut chart
-  // // You can switch between pie and douhnut using the method below.
-  // pieChart.Doughnut(PieData, pieOptions);
 
   //-------------
   //- BAR CHART -
   //-------------
   var barChartCanvas = $("#barChart").get(0).getContext("2d");
+
+
   var barChart = new Chart(barChartCanvas);
   var barChartData = areaChartData;
   barChartData.datasets[1].fillColor = "#00a65a";
